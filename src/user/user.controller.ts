443cf6562487086d88a0a47337c9d510688f5a9e
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-08-11 14:35:39 -0300

    :sparkles: feat: implement guards and jwt strategy

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import * as client from 'generated/prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: client.User) {
+    return user;
   }
 }
