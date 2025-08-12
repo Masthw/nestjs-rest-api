9f40669274b0b20462946c1f3a65284afcfca71a
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-08-12 10:43:38 -0300

    :test_tube: test: implement auth tests e2e

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,12 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+
+  cleanDb() {
+    // transaction execute the queries in order, so the bookmarks are deleted before the users
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
